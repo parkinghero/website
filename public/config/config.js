70d9548c1b3ee5791a4e6885019b265ffdaa807f
koma <maks@koma.org.ua>
2015-04-19 09:52:48 +0300

    config

---
+++ public/config/config.js
@@ -0,0 +1,11 @@
+;(function(window) {
+  // var api = "http://api.parkinghero.in.ua";
+  var api = "http://localhost:3000";
+
+  window.config = {
+    apiUrl: api,
+    photosUrl: api + "/photos",
+    issuesUrl: api + "/issues"
+  };
+})(window);
+
